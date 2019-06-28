332f58ee295fa03b69c0ea6cb4d3ab97f42343ad
Stud <sff19931109@163.com>
2019-06-28 21:48:01 +0800

    update

--- Travel/scenic/views.py
+++ Travel/scenic/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from . import models
+from django.http import HttpResponse
+import re
 
 # Create your views here.
 # Create your views here.
@@ -11,3 +14,12 @@ def ticket(request):
 def scenic2(request):
     if request.method == 'GET':
         return render(request,'scenic/information02.html')
+def add_s1(request):
+    db1=open('/home/tarena/Letu1.0/Travel/static/images/scenic/text/s1.txt','r+')
+    for line in db1:
+        list1 = line.split('#')
+        scen1 = models.Scen(id=list1[0], sce_name=list1[1],sce_topic=list1[2], brief_des=list1[3], bus_go=list1[4],
+                            car_go=list1[5],pre_price=list1[6],local_price=list1[7],low_time=list1[8])
+        scen1.save()
+    db1.close()
+    return HttpResponse('插入成功')
