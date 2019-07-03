79a901b06d4c39c382bcc0e0162703f44d191bfa
Stud <sff19931109@163.com>
2019-07-03 19:32:29 +0800

    kkkk

--- Travel/scenic/urls.py
+++ Travel/scenic/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    # url(r'^adds1$',views.add_s1),
-    # url(r'^adds2$',views.add_s2),
-    # url(r'^addtic$',views.add_tic),
-    # url(r'^addintro$',views.add_intro),
+    url(r'^adds1$',views.add_s1),
+    url(r'^adds2$',views.add_s2),
+    url(r'^addtic$',views.add_tic),
+    url(r'^addintro$',views.add_intro),
     url(r'^$',views.index,name='scenic1'),
     url(r'(\d+)/$',views.ticket),
     url(r'^scenic2$',views.scenic2,name='scenic2'),
