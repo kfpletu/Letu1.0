24818c8b5e36cb7487419d04b13bad8f585b7c4c
Tedu <330150956@qq.com>
2019-07-04 09:45:53 +0800

    酒店数据库

--- Travel/hotel/views.py
+++ Travel/hotel/views.py
@@ -99,9 +99,8 @@ def room(request):
 
 #导入酒店数据
 def init_hotel(request):
-            pwd=os.path.dirname(__file__)
-            hotel_db_path=os.path.join(pwd,'db.txt')
-            fd=open("/home/tarena/桌面/test/中期项目/letu/Travel/static/images/hotel/db2.txt",'r+')
+
+            fd=open("/home/tarena/桌面/test/中期项目/Letu1.0/Travel/static/images/hotel/hotels.txt",'r+')
             for line in fd:
                 # line=fd.read()
                 # line="32#'富力希尔顿大酒店'#'新城区东新街199号（近民乐园）'#'029-87388888'#'西安富力希尔顿酒店坐落于古城西安市中心，周边繁华购物区林立，是商务出行和休闲度假的理想之选。步行即可到达闻名于世的明城墙和西安地标建筑钟楼。乘坐出租车仅需5分钟即可到达东大街、骡马市步行街及回民街，这里有西安特色的餐厅、购物及娱乐场所。 拥有城中稀缺宽敞大容量客房，西安富力希尔顿酒店客房面积均大于43平方米。酒店高雅的客房别出心裁将唐朝元素融入其中，并突出了房间的温馨舒适。所有客房均配备高速宽带无线上网及高水准的客用品。 西安富力希尔顿酒店将成为您在西安的会议和用餐新选择。三间风格各异的餐厅及酒吧、一间1200平方米的大宴会厅及6间多功能厅可为您提供多种规模的会议及宴会需求。'#'星级酒店'"
@@ -111,9 +110,37 @@ def init_hotel(request):
                 hotel.save()
             fd.close()
             return HttpResponse('初始化成功')
+
+#初始化房间
+def init_room(request):
+    fd = open("/home/tarena/桌面/test/中期项目/Letu1.0/Travel/static/images/hotel/room.txt", 'r+')
+    for line in fd:
+        # line=fd.read()
+        # line="32#'富力希尔顿大酒店'#'新城区东新街199号（近民乐园）'#'029-87388888'#'西安富力希尔顿酒店坐落于古城西安市中心，周边繁华购物区林立，是商务出行和休闲度假的理想之选。步行即可到达闻名于世的明城墙和西安地标建筑钟楼。乘坐出租车仅需5分钟即可到达东大街、骡马市步行街及回民街，这里有西安特色的餐厅、购物及娱乐场所。 拥有城中稀缺宽敞大容量客房，西安富力希尔顿酒店客房面积均大于43平方米。酒店高雅的客房别出心裁将唐朝元素融入其中，并突出了房间的温馨舒适。所有客房均配备高速宽带无线上网及高水准的客用品。 西安富力希尔顿酒店将成为您在西安的会议和用餐新选择。三间风格各异的餐厅及酒吧、一间1200平方米的大宴会厅及6间多功能厅可为您提供多种规模的会议及宴会需求。'#'星级酒店'"
+        list = line.split('#')
+        room = models.Room(room_name=list[0], area=list[1], price=list[2], window=list[3],
+                             bed=list[4])
+        room.save()
+    fd.close()
+    return HttpResponse('初始化成功')
+
+
+#导入图片地址
+def house_p(request):
+    houses=models.House.objects.all()
+    for house in houses:
+        a=house.id//10
+        b=house.id%10
+        house.hotel_p="%s/%s"%(a,b)
+        house.save()
+    return HttpResponse('图片地址初始化')
+
+
+
+
 #备份房间数据
 def backup(request):
-    fd = open("/home/tarena/桌面/test/中期项目/letu/Travel/static/images/hotel/room.txt", 'r+')
+    fd = open("/home/tarena/桌面/test/中期项目/Letu1.0/Travel/static/images/hotel/room.txt", 'r+')
     rooms=models.Room.objects.all()
     for room in rooms:
         str=room.room_name+'#'+room.area+'#'+room.price+'#'+room.window+'#'+room.bed+'\n'
@@ -132,22 +159,18 @@ def init_house_id(request):
     models.Room.objects.all().update(house_id=F('hotel_id'))
     models.Hotel.objects.all().update(house_id=F('id'))
     return HttpResponse('初始化成功')
+
 #设置room_iprice
 def init_house_iprice(requset):
     rooms=models.Room.objects.all()
     for room in rooms:
-        room.room_p=room.hotel.hotel_p
+        room.iprice=int(room.price)
         room.save()
-
-
-def b_rooms():
-    for room in models.Room.objects.all():
-        fd=open('/home/tarena/桌面/test/中期项目/Letu1.0/Travel/static/images/hotel/rooms.txt')
-
+    return HttpResponse('价位设置成功')
 
 # 数据库主表数据导入
 def init_house(request):
-    fd = open("/home/tarena/桌面/test/中期项目/letu/Travel/static/images/hotel/house.txt", 'r+')
+    fd = open("/home/tarena/桌面/test/中期项目/Letu1.0/Travel/static/images/hotel/house.txt", 'r+')
     for line in fd:
         list = line.split('#')
         hotel = models.House(id=list[0],hotel_name=list[1])
@@ -158,10 +181,10 @@ def init_house(request):
 #详情表数据函数
 def hotel_ticket(id):
     id = int(id)
-    order_count = models.House.objects.get(id=id).order_count
     hotel = models.Hotel.objects.get(id=id)
+    order_count = hotel.house.order_count
     hotel_name = hotel.hotel_name  # '富力希尔顿大酒店'
-    hotel_p = hotel.hotel_p
+    hotel_p = hotel.house.hotel_p
     phone = hotel.phone  # '029-87388888'
     address = hotel.address  # '新城区东新街199号（近民乐园）'
     infor = hotel.info  # '西安富力希尔顿酒店坐落于古城西安市中心，周边繁华购物区林立，是商务出行和休闲度假的理想之选。步行即可到达闻名于世的明城墙和西安地标建筑钟楼。乘坐出租车仅需5分钟即可到达东大街、骡马市步行街及回民街，这里有西安特色的餐厅、购物及娱乐场所。 拥有城中稀缺宽敞大容量客房，西安富力希尔顿酒店客房面积均大于43平方米。酒店高雅的客房别出心裁将唐朝元素融入其中，并突出了房间的温馨舒适。所有客房均配备高速宽带无线上网及高水准的客用品。 西安富力希尔顿酒店将成为您在西安的会议和用餐新选择。三间风格各异的餐厅及酒吧、一间1200平方米的大宴会厅及6间多功能厅可为您提供多种规模的会议及宴会需求。'
