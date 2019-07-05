062ef37162315fa08efba71dd9dfc2cac8710220
Wmh <1308732441@qq.com>
2019-07-05 14:04:33 +0800

    1

--- Travel/static/js/user/login.js
+++ Travel/static/js/user/login.js
@@ -1,6 +1,4 @@
 $(function() {
-
-    
     $('.login').click(function() {
         if ($('#show').html() ||!$('#validateCode').val()){
             alert('请输入验证码')
@@ -25,52 +23,4 @@ $(function() {
             });
         }
     });
-
-    $('#yanzma').click(function() {
-        $.ajax({
-            type: "get",
-            url: "/user/yanzma",
-            dataType:'json',
-            success: function(response) {
-                $('#showImg').html(response.code)
-                $('#yanzma').attr('src',response.url+"?t="+Math.random())
-            },
-            error: function(){
-                alert('验证码加载失败')
-                location.href='/user/login'
-            }
-            
-        });
-    })
-
-    $('#validateCode').blur(function(){
-        code = $('#showImg').html()
-        inputCode = $('#validateCode').val()
-        if(inputCode){
-            if(code == inputCode){
-                $('#show').html('')
-            }else{
-                $('#show').html('验证码错误,请重新输入')
-                $.ajax({
-                    type: "get",
-                    url: "/user/yanzma",
-                    dataType:'json',
-                    success: function(response) {
-                        $('#showImg').html(response.code)
-                        $('#yanzma').attr('src',response.url+"?t="+Math.random())
-                    },
-                    error: function(){
-                        alert('验证码加载失败')
-                        location.href='/user/login'
-                    }
-                    
-                });
-            }
-        }else{
-            if($('#yanzma').attr('src')){
-                $('#show').html('请输入验证码')
-            }
-        }
-    })
-    
 })
